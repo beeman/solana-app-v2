69a51f5142ef9d906cc18bb2795f6b79058866d2
Bram Borggreve <beeman@beeman.nl>
2024-11-09 02:52:07 -0500

    feat: add shadcn/ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,19 @@
+import { AppDemoDialog } from "@/app-demo-dialog.tsx";
+import { ModeToggle } from "@/components/mode-toggle.tsx";
+import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { Button } from "@/components/ui/button.tsx";
 
 export function App() {
 
   return (
-    <div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <h1 className="text-3xl font-bold underline">
         Vite + React + Tailwind
       </h1>
-    </div>
+      <Button>Click me</Button>
+
+      <ModeToggle/>
+      <AppDemoDialog />
+    </ThemeProvider>
   )
 }
-
-export default App
